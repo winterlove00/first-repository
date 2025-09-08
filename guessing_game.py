4338d9d9991d7926c43cbd44d574274cba7cfd48
winterlove00 <junhyup6946@gmail.com>
2025-09-08 15:38:36 +0900

    4 files are committed

---
+++ guessing_game.py
@@ -0,0 +1,9 @@
+import random
+
+number = random.randint(1, 10)
+guess = int(input("1부터 10 사이의 숫자를 맞춰보세요: "))
+
+if guess == number:
+    print("정답입니다!")
+else:
+    print(f"틀렸습니다. 정답은 {number}였습니다.")
